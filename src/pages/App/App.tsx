471f86f470e85f185837e78928cd672c6f6659ae
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-20 19:03:23 +0100

    brought the phone svg still working on the initial design

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -2,13 +2,21 @@ import './app.scss';
 // import { motion } from 'framer-motion';
 // import { gsap } from 'gsap';
 
+
+// importing of all assets
+import phone from '../../assets/svg/p1.svg'
+
 const App = () => {
 
     return (
         <div className="AppMain">
-            <div className="AppTitle">
-                <p>Your dream</p>
-                <p>Meet the new iPhone 14 pro</p>
+            <div className="AppTCvr">
+                <div className="AppTitle">
+                    <p>Your dream</p>
+                    <p>Meet the new iPhone 14 pro</p>
+                </div>
+            </div>
+            <div className='boy'>
             </div>
         </div>
     )
